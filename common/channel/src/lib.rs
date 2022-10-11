e20f3913f9cfab271e38485d4b45bce2861c240e
MiniFrenchBread <103425574+MiniFrenchBread@users.noreply.github.com>
2022-10-11 18:42:55 +0800

    chore: update dependencies

--- common/channel/src/lib.rs
+++
@@ -1,4 +0,0 @@
-mod channel;
-pub mod error;
-
-pub use crate::channel::{Channel, Message, Receiver, ResponseSender, Sender};
--- common/contract-interface/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use ethers::prelude::abigen;
-
-// run `cargo doc -p contract-interface --open` to read struct definition
-
-abigen!(
-    IonianFlow,
-    "../../ionian-contracts-poc/artifacts/contracts/dataFlow/Flow.sol/Flow.json"
-);
